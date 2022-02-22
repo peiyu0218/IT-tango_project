e112090aa332343440653f4e5c018751d8db7c52
Peiyu Jiang <2704229j@student.gla.ac.uk>
2022-02-22 22:59:08 +0000

    chap3

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+def index(request):
+    res = HttpResponse("Rango says hey there partner!!")
+    res.write("<a href='/rango/about/'>About</a>")
+    return res
+def about(request):
+    res = HttpResponse("Rango says here is the about page.")
+    res.write("<a href='/rango/'>Index</a>")
+    return res
